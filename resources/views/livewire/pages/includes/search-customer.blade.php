5fb9a67fb0410a2a7ebaa6ab0cac4227b90e9eae
root <jamesmtungi2013@gmail.com>
2025-01-19 10:30:02 +0300

    status

--- resources/views/livewire/pages/includes/search-customer.blade.php
+++ resources/views/livewire/pages/includes/search-customer.blade.php
@@ -57,12 +57,14 @@
                                 Prepaid payment
                             </th>
                             <th scope="col" class="p-4 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-400">
-                                status
+                               Order status
                             </th>
 
                             <th scope="col" class="p-4 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-400">
-                               materials
+                               Payment status
                             </th>
+
+                          
 
                            
 
@@ -93,12 +95,32 @@
                             <td class="p-4 text-base font-medium text-gray-900 whitespace-nowrap dark:text-white">{{number_format($member->expenses)}}</td>
                             <td class="p-4 text-base font-medium text-gray-900 whitespace-nowrap dark:text-white">{{number_format($member->prepaid)}}</td>
 
-                            <td class="p-4  space-x-2 whitespace-nowrap">
-                                <class   id="defaultModalButton" data-modal-target="defaultModal" data-modal-toggle="" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white rounded-lg bg-yellow-500 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
-                                    <svg class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z"></path><path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd"></path></svg>
-                                    Pending
-                                </class>
-                            <td class="p-4 text-base font-medium text-gray-900 whitespace-nowrap dark:text-white">{{$member->materials}}</td>
+                            <td class="p-4 space-x-2 whitespace-nowrap">
+    <span 
+        id="defaultModalButton" 
+        data-modal-target="defaultModal" 
+        data-modal-toggle="" 
+        class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white rounded-lg 
+            {{ $member->status === 'pending' ? 'bg-yellow-400 hover:bg-yellow-500' : 'bg-green-500 hover:bg-green-600' }} 
+            focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-800"
+    >
+        <svg class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+            <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z"></path>
+            <path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd"></path>
+        </svg>
+        {{$member->status}}
+    </span>
+</td>
+<td class="p-4 text-base font-medium whitespace-nowrap">
+            @if($member->prepaid == $member->work_cost)
+                <span class="px-3 py-1 text-sm font-medium text-white bg-green-500 rounded-lg">No Debt</span>
+            @elseif($member->prepaid < $member->work_cost)
+                <span class="px-3 py-1 text-sm font-medium text-white bg-yellow-500 rounded-lg">Claimed</span>
+            @else
+                <span class="px-3 py-1 text-sm font-medium text-white bg-gray-500 rounded-lg">Overpaid</span>
+            @endif
+        </td>
+                            <!-- <td class="p-4 text-base font-medium text-gray-900 whitespace-nowrap dark:text-white">{{$member->materials}}</td> -->
 
                             <!-- <td class="p-4 text-base font-normal text-gray-900 whitespace-nowrap dark:text-white">
                                 <div class="flex items-center">
