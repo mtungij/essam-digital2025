a801100350e0929c36043362cfc55dac020a491e
root <jamesmtungi2013@gmail.com>
2025-03-01 09:05:57 +0300

    first

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
+    ->middleware(['auth'])
     ->name('dashboard');
 
 Route::view('profile', 'profile')
